d73f29a80f76557a6e3a6d01c51d017ace837f96
Ilya <ibequa@gmail.com>
2017-03-06 21:58:36 +0300

    Project structure changes

--- app/src/main/java/com/vectortwo/healthkeeper/activities/MainActivity.java
+++ app/src/main/java/com/vectortwo/healthkeeper/activities/MainActivity.java
@@ -1,7 +1,8 @@
-package com.vectortwo.healthkeeper;
+package com.vectortwo.healthkeeper.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.vectortwo.healthkeeper.R;
 
 public class MainActivity extends AppCompatActivity {
 
